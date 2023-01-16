0a618e4ad53ff729a8ed07f63ed303c286efe1af
wxxk <dwde22@gmail.com>
2023-01-16 22:53:37 +0900

    chap1_07

--- src/chap_01/_07_TypeCasting.java
+++ src/chap_01/_07_TypeCasting.java
@@ -31,5 +31,26 @@ public class _07_TypeCasting {
 
         int convertedScoreInt = (int) score_d; // 191.8 -> 191
         // double -> float -> long -> int : 수동으로 형변환
+
+        // 숫자를 문자열로
+        String s1 = String.valueOf(93);
+        s1 = Integer.toString(93);
+        System.out.println(s1); // 93
+
+        String s2 = String.valueOf(98.8);
+        s2 = Double.toString(98.8);
+        System.out.println(s2); // 98.8
+
+        // 문자열을 숫자로
+        int i = Integer.parseInt("93");
+        System.out.println(i); // 93
+        double d = Double.parseDouble("98.8");
+        System.out.println(d); // 98.8
+
+        /*
+        int error = Integer.parseInt("자바");
+        System.out.println(error);
+         */
+
     }
 }
