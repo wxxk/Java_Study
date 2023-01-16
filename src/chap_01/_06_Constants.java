9a42b9ba5f6cc5adb4711d72427c25f0bc24e3e7
wxxk <dwde22@gmail.com>
2023-01-16 22:34:31 +0900

    chap1_06

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        // 키워드 : 자바에서 약속된 몇 가지의 단어
+        // 상수는 모두 대분자 / 2개 이상 단어 _로 구분
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호
+        // KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.14; // 원주율
+        final String DATE_OF_BIRTH = "1996-11-23"; // 생년월일
+    }
+}
