e8cee74eae7fb8d0c567e700b9332635116459c1
wxxk <dwde22@gmail.com>
2023-02-28 22:04:03 +0900

    chap4_8. 이중 반복문

---
+++ src/chap_04/_08_NestedLoop.java
@@ -0,0 +1,57 @@
+package chap_04;
+
+public class _08_NestedLoop {
+    public static void main(String[] args) {
+        // 이중 반복문
+
+        // 별 (*) 사각형 만들기
+        /*
+
+        *****
+        *****
+        *****
+        *****
+        *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println(" ------------------- ");
+
+        // 별 (*) 왼쪽 삼각형 만듥리
+        for (int i = 0; i < 5; i++) {
+            // for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println(" ------------------- ");
+
+        // 별 (*) 오즌쪽 삼격형 만들기
+        /*
+
+            *
+           **
+          ***
+         ****
+        *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < (5 - i -1); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i ; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
